0e99d1192a617e035016809b74cde7e425fe70e9
Benjamín Gautier <benjamin.gautier.14@sansano.usm.cl>
2021-07-12 23:21:06 -0400

    create template skeleton

--- formlyui/src/app/app.component.ts
+++ formlyui/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   isCollapsed = false;
+
+  toggleActive = true;
+
+  constructor(
+    @Inject(DOCUMENT) private doc: Document
+  ) {
+
+  }
+
+  toggleDarkMode() {
+    this.toggleActive = !this.toggleActive;
+    if (this.toggleActive) {
+      this.doc.getElementsByTagName('html').item(0)?.classList.add('dark');
+      this.doc.getElementsByTagName('html').item(0)?.classList.remove('bg-white');
+    } else {
+      this.doc.getElementsByTagName('html').item(0)?.classList.add('bg-white');
+      this.doc.getElementsByTagName('html').item(0)?.classList.remove('dark');
+    }
+  }
 }
