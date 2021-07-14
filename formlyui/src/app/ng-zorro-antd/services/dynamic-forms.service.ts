c4abe6e159475ac2646dcee876314a0e19835d89
Benjamín Gautier <benjamin.gautier.14@sansano.usm.cl>
2021-07-14 17:23:11 -0400

    declare updateField to add new item -- next to refactor

--- formlyui/src/app/ng-zorro-antd/services/dynamic-forms.service.ts
+++ formlyui/src/app/ng-zorro-antd/services/dynamic-forms.service.ts
@@ -2,14 +2,22 @@ import { Injectable } from '@angular/core';
 import { FormlyFieldConfig } from '@ngx-formly/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 
+export interface NewFormlyFieldConfig {
+  index?: number;
+  config?: FormlyFieldConfig
+} 
+
 @Injectable({
   providedIn: 'root'
 })
 export class DynamicFormsService {
-  private _onUpdateFields: BehaviorSubject<FormlyFieldConfig> = new BehaviorSubject({});
+  private _onUpdateFields: BehaviorSubject<NewFormlyFieldConfig> = new BehaviorSubject({});
 
-  public readonly onUpdateFields: Observable<FormlyFieldConfig> = this._onUpdateFields.asObservable();
+  public readonly onUpdateFields: Observable<NewFormlyFieldConfig> = this._onUpdateFields.asObservable();
 
   constructor() { }
 
+  public updateField(newItem: NewFormlyFieldConfig) {
+    this._onUpdateFields.next(newItem);
+  }
 }
