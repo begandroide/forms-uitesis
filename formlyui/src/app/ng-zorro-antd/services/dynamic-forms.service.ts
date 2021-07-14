79658c51031d01b4a1108b0c83a001489edda0fc
Benjamín Gautier <benjamin.gautier.14@sansano.usm.cl>
2021-07-13 22:17:09 -0400

    declare _onUpdateFields dynamc forms service

--- formlyui/src/app/ng-zorro-antd/services/dynamic-forms.service.ts
+++ formlyui/src/app/ng-zorro-antd/services/dynamic-forms.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { FormlyFieldConfig } from '@ngx-formly/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DynamicFormsService {
+  private _onUpdateFields: BehaviorSubject<FormlyFieldConfig> = new BehaviorSubject({});
+
+  public readonly onUpdateFields: Observable<FormlyFieldConfig> = this._onUpdateFields.asObservable();
 
   constructor() { }
+
 }
