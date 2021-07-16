78df61e69b0b88f2068ecd37eaf7c46e80d18c9d
Benjamín Gautier <benjamin.gautier.14@sansano.usm.cl>
2021-07-15 21:57:57 -0400

    support new skeleton layout and provide matcher media service

--- formlyui/src/app/form-builder/form-builder.module.ts
+++ formlyui/src/app/form-builder/form-builder.module.ts
@@ -5,6 +5,7 @@ import { FormBuilderRoutingModule } from './form-builder-routing.module';
 import { FormBuilderComponent } from './form-builder.component';
 import { SkeletonModule } from './components/';
 import { FuseSidebarModule } from './components/sidebar/sidebar.module';
+import { MediaMatcherService } from './shared/services/media-matcher.service';
 
 
 @NgModule({
@@ -16,6 +17,9 @@ import { FuseSidebarModule } from './components/sidebar/sidebar.module';
     FormBuilderRoutingModule,
     SkeletonModule,
     FuseSidebarModule
+  ],
+  providers: [
+    MediaMatcherService
   ]
 })
 export class FormBuilderModule { }
