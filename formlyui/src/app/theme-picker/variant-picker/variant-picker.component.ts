4d4a7f46c51b6faa070903a00912fa790c289f13
Benjamín Gautier <benjamin.gautier.14@sansano.usm.cl>
2021-07-16 17:23:04 -0400

    declare variant-picker component

---
+++ formlyui/src/app/theme-picker/variant-picker/variant-picker.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-variant-picker',
+  templateUrl: './variant-picker.component.html',
+  styleUrls: ['./variant-picker.component.scss']
+})
+export class VariantPickerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
