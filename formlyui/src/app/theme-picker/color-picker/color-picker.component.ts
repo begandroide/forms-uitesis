417427068ae449eec1deafb5afa26f3fa02e5987
Benjamín Gautier <benjamin.gautier.14@sansano.usm.cl>
2021-07-16 19:01:20 -0400

    color picker has already a selectable item and eventEmitter of change.

--- formlyui/src/app/theme-picker/color-picker/color-picker.component.ts
+++ formlyui/src/app/theme-picker/color-picker/color-picker.component.ts
@@ -1,4 +1,13 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { generate, presetPalettes } from '@ant-design/colors';
+
+const Colors = [
+  "#000000", "#f44336", "#e91e63", "#9c27b0", "#673ab7", "#3f51b5", "#2196f3", "#03a9f4",
+  "#00bcd4", "#009688", "#4caf50", "#8bc34a", "#cddc39", "#ffeb3b", "#ffc107", "#ff9800",
+  "#ff5722", "#795548",
+];
+
+const DefaultColor = '#000000';
 
 @Component({
   selector: 'app-color-picker',
@@ -7,70 +16,30 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ColorPickerComponent implements OnInit {
-  colors = [
-    {
-      color: "#000",
-      selected: true
-    },{
-      color: "#f44336",
-      selected: false
-    },{
-      color: "#e91e63",
-      selected: false
-    },{
-      color: "#9c27b0",
-      selected: false
-    },{
-      color: "#673ab7",
-      selected: false
-    },{
-      color: "#3f51b5",
-      selected: false
-    },{
-      color: "#2196f3",
-      selected: false
-    },{
-      color: "#03a9f4",
-      selected: false
-    },{
-      color: "#00bcd4",
-      selected: false
-    },{
-      color: "#009688",
-      selected: false
-    },{
-      color: "#4caf50",
-      selected: false
-    },{
-      color: "#8bc34a",
-      selected: false
-    },{
-      color: "#cddc39",
-      selected: false
-    },{
-      color: "#ffeb3b",
-      selected: false
-    },{
-      color: "#ffc107",
-      selected: false
-    },{
-      color: "#ff9800",
-      selected: false
-    },{
-      color: "#ff5722",
-      selected: false
-    },{
-      color: "#795548",
-      selected: false
-    },{
-      color: "#607d8b",
-      selected: false
-    },
-  ];
+  @Input() selectedColor: string = DefaultColor;
+  @Output() generatedColorsChange: EventEmitter<string[]>;
+  
+  colors = Colors;
+  generatedColors: string[] = [];
+  selectedIndex = 0;
 
-  constructor() { }
-
+  constructor() { 
+    this.generatedColorsChange = new EventEmitter();
+  }
+  
   ngOnInit(): void {
+    this.generateColors(this.selectedColor);
   }
 
+  changeToColor(index: number) {
+    if (this.selectedIndex !== index) {
+      this.selectedIndex = index;
+      this.generateColors(this.colors[index]);
+    }
+  }
+  
+  private generateColors(hex?: string) {
+    this.generatedColors = generate(hex ?? DefaultColor);
+    this.generatedColorsChange.emit(this.generatedColors);
+  }
 }
