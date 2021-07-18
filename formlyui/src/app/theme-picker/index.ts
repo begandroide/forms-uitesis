9e99025bb9b68e90b96e987434a5c415774a001e
Benjamín Gautier <benjamin.gautier.14@sansano.usm.cl>
2021-07-18 17:16:02 -0400

    Auto-generated commit [ci skip]

--- formlyui/src/app/theme-picker/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './services/theme-picker.service';
-export * from './color-picker/color-picker.component';
-export * from './variant-picker/variant-picker.component';
-export * from './const';
