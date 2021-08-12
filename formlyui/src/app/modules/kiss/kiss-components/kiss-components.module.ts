c8ab3b13239677bc78c8e691377fb00252f8e5f0
Benjamín Gautier <benjamin.gautier.14@sansano.usm.cl>
2021-08-12 16:40:54 -0400

    declare kiss-survey-item entity class with guid-generator

--- formlyui/src/app/modules/kiss/kiss-components/kiss-components.module.ts
+++ formlyui/src/app/modules/kiss/kiss-components/kiss-components.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { KissInputModule } from './kiss-input/kiss-input.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    KissInputModule
+  ],
+  exports: [
+    KissInputModule
   ]
 })
 export class KissComponentsModule { }
