7bc8f29a91b5f40db3e737ebf215102320bdb361
Benjamín Gautier <benjamin.gautier.14@sansano.usm.cl>
2021-08-12 00:01:45 -0400

    listen to change selected option from kiss-survey-control-selector and add to questionList

--- formlyui/src/app/modules/kiss/kiss-survey-creator/kiss-survey-creator.component.ts
+++ formlyui/src/app/modules/kiss/kiss-survey-creator/kiss-survey-creator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { KissControlOption } from './shared/kiss-control-types';
 
 @Component({
   selector: 'app-kiss-survey-creator',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KissSurveyCreatorComponent implements OnInit {
 
+  questionList: KissControlOption[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onOptionChanged(event: KissControlOption) {
+    console.log(event);
+    // IF event value is not contained in questionList, add it
+    if (this.questionList.indexOf(event) === -1) {
+      this.questionList.push(event);
+    }
+  }
 }
