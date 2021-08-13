1243a4a141274b7d5e6094f7f74693e061b26b6c
Benjamín Gautier <benjamin.gautier.14@sansano.usm.cl>
2021-08-12 21:28:23 -0400

    welcome kiss-components selector group

--- formlyui/src/app/modules/kiss/kiss-survey-creator/shared/kiss-control-types.ts
+++ formlyui/src/app/modules/kiss/kiss-survey-creator/shared/kiss-control-types.ts
@@ -1,4 +1,9 @@
 
+export interface KissControlDefinition {
+    label: string;
+    controls: KissControlOption[];
+}
+
 export interface KissControlOption {
     value: KissControlType;
     label: string;
@@ -8,32 +13,44 @@ export interface KissControlOption {
 export enum KissControlType {
     Radio = 'radio',
     Checkbox = 'checkbox',
-    Select = 'select',
     Text = 'text',
     Textarea = 'textarea',
-    Date = 'date',
-    Time = 'time',
-    DateTime = 'datetime',
-    Email = 'email',
     Number = 'number',
-    Url = 'url',
-    Image = 'image',
-    File = 'file'
+    Likert3 = 'likert3',
+    Likert5 = 'likert5',
 }
 
-export const KISS_CONTROL_TYPES: KissControlOption[] = [
-    { value: KissControlType.Radio, label: 'Radio', icon: 'radio' },
-    { value: KissControlType.Checkbox, label: 'Checkbox', icon: 'checkbox' },
-    { value: KissControlType.Select, label: 'Select', icon: 'select' },
-    { value: KissControlType.Text, label: 'Text', icon: 'text' },
-    { value: KissControlType.Textarea, label: 'Textarea', icon: 'textarea' },
-    { value: KissControlType.Date, label: 'Date', icon: 'date' },
-    { value: KissControlType.Time, label: 'Time', icon: 'time' },
-    { value: KissControlType.DateTime, label: 'DateTime', icon: 'datetime' },
-    { value: KissControlType.Email, label: 'Email', icon: 'email' },
-    { value: KissControlType.Number, label: 'Number', icon: 'number' },
-    { value: KissControlType.Url, label: 'URL', icon: 'url' },
-    { value: KissControlType.Image, label: 'Image', icon: 'image' },
-    { value: KissControlType.File, label: 'File', icon: 'file' }
+export const KISS_CONTROL_TYPES: KissControlDefinition[] = [
+    {
+        label: 'Texto libre',
+        controls: [
+            { value: KissControlType.Text, label: 'Text', icon: 'text' },
+            { value: KissControlType.Textarea, label: 'Textarea', icon: 'textarea' }
+        ]
+    },
+    {
+        label: 'Número/cantidad',
+        controls: [
+            { value: KissControlType.Number, label: 'Number', icon: 'number' },
+        ]
+    },
+    {
+        label: 'Alternativas',
+        controls: [
+            { value: KissControlType.Radio, label: 'Radio', icon: 'radio' },
+            { value: KissControlType.Checkbox, label: 'Checkbox', icon: 'checkbox' },
+        ]
+    },
+    {
+        label: 'Número',
+        controls: [
+        ]
+    },
+    {
+        label: 'Likert',
+        controls: [
+            { value: KissControlType.Likert3, label: '3-Likert', icon: 'select' },
+            { value: KissControlType.Likert5, label: '5-Likert', icon: 'select' },
+        ]
+    }
 ];
-
